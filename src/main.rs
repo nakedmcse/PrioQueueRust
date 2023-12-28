e2423d5f17f3e80627c51c7983f9e8d500af0136
Walker Aldridge <daldridge@lairdscomputer.com>
2023-12-28 16:25:36 -0600

    Fix issue with uniqueness

--- src/main.rs
+++ src/main.rs
@@ -4,32 +4,36 @@ use std::time::Instant;
 use rand::Rng;
 
 fn main() {
-    let range: i64 = 10000000;
+    let range: u64 = 10000000;
     let mut rng = rand::thread_rng();
-    let mut simple_prio = PriorityQueue::new();
+    let mut simple_prio: PriorityQueue<(i64,u64), i64> = PriorityQueue::new();
     let mut test_out = String::new();
 
     // Test dequeue order
-    simple_prio.push(1,1);
-    simple_prio.push(2,1);
-    simple_prio.push(3,1);
-    simple_prio.push(4,5);
-    simple_prio.push(5,9);
+    simple_prio.push((1,1),1);
+    simple_prio.push((2,2),1);
+    simple_prio.push((3,3),1);
+    simple_prio.push((4,4),5);
+    simple_prio.push((5,5),9);
 
     while !simple_prio.is_empty() {
-        test_out = [test_out, simple_prio.pop().unwrap_or((0,0)).0.to_string()].join(" ");
+        test_out = [test_out, simple_prio.pop().unwrap_or(((0,0),0)).0.0.to_string()].join(" ");
     }
 
     println!("Simple Priority Queue Dequeued: {}",test_out);
     
     // Speed Test Enqueue
     let mut pairs: Vec<(i64, i64)> = Vec::new();
-    for n in 1..range {
-        pairs.push((rng.gen_range(0..range),rng.gen_range(1..9)));
+    let mut n:u64 = 0;
+    while n<range {
+        pairs.push((rng.gen_range(0..10000000),rng.gen_range(1..9)));
+        n += 1;
     }
     let start_enqueue = Instant::now();
+    n = 0;
     for pair in pairs {
-        simple_prio.push(pair.0,pair.1);
+        simple_prio.push((pair.0,n),pair.1);
+        n += 1;
     }
     let duration_enqueue = start_enqueue.elapsed();
     println!("Simple Priority Queue Enqueue time: {:?}", duration_enqueue);
